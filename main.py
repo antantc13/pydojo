099223eebd1a3220ff224a89fe343c42706ae32e
antantc13 <antantc13@gmail.com>
2024-07-23 02:23:48 -0700

    new algorithm

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-for i in range(1, 10):
-    print("115-to.y")
+'''
+We want to iterate through all numbers between 1 and 100, and print that nuber if it is a multiple of 3 or 5.
+'''
+
+
+for i in range(1, 101):
+    if i % 3 == 0 or i % 5 == 0:
+        print(i)
